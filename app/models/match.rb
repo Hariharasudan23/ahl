8172f0a846500730f974816c808bb32d6a43fd1e
Arun Kumar <arunkumarm1995@gmail.com>
2017-01-04 20:56:32 +0530

    resolve hound warnings

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,12 +4,10 @@ class Match < ActiveRecord::Base
 
   # Returns readable format of url
   def match_url
-    self.first_team.name + " vs " + self.opponent_team.name + " " + self.time.strftime("%b %d")
+    "#{first_team.name} vs #{opponent_team.name} #{time.strftime('%b %d')}"
   end
 
-
-  def should_generate_new_friendly_id?
-  end
+  def should_generate_new_friendly_id?; end
 
   scope :ended, -> { Match.where.not(result: -2) }
 
@@ -22,28 +20,28 @@ class Match < ActiveRecord::Base
 
   # Validations
 
-  validates :result, presence: true, inclusion: {in: [-2, -1, 0, 1]}
+  validates :result, presence: true, inclusion: { in: [-2, -1, 0, 1] }
   validate :different_teams
   # validate :wrong_player
   validates :team1_id, presence: true
   validates :team2_id, presence: true
-  # don't uncomment the below two lines since the validations would fire up in other forms
+  # don't uncomment the below two lines since the validations would
+  # fire up in other forms
   # validates :trump_card, presence: true, numericality: true
   # validates :man_of_the_match, presence: true, numericality: true
 
-
   # To retrieve the report associated with a match
   def report
-    Report.where(match_id: self.id)
+    Report.where(match_id: id)
   end
 
   # Retriving the teams of a particular match
   def first_team
-    Team.find(self.team1_id)
+    Team.find(team1_id)
   end
 
   def opponent_team
-    Team.find(self.team2_id)
+    Team.find(team2_id)
   end
 
   def ended?
@@ -51,14 +49,13 @@ class Match < ActiveRecord::Base
   end
 
   def winner
-    result = self.result
     if result != -2
       case result
         # First team's the winner
       when 1
-        final_result = Team.find(self.team1_id).name
+        final_result = Team.find(team1_id).name
       when -1
-        final_result = Team.find(self.team2_id).name
+        final_result = Team.find(team2_id).name
       when 0
         final_result = 'Draw'
       end
@@ -71,9 +68,8 @@ class Match < ActiveRecord::Base
   end
 
   # Custom validations
-
   def different_teams
-    if self.team1_id == self.team2_id
+    if team1_id == team2_id
       errors.add(:team2_id, "can't be the same as Team1")
     end
   end
@@ -83,7 +79,8 @@ class Match < ActiveRecord::Base
   # def wrong_player
   #   team = Player.find(self.man_of_the_match).team_id
   #   if team != self.team1_id and team != self.team2_id
-  #     errors.add(:man_of_the_match, "should belong to one of the two teams. Check the man_of_the_match id")
+  #     errors.add(:man_of_the_match,
+  #       'should belong to one of the two teams. Check man_of_the_match id')
   #   end
   # end
 end
