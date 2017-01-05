12c371e306b17dad5c715ea80d54cc49df62acb3
Arun Kumar <arunkumarm1995@gmail.com>
2017-01-05 12:34:46 +0530

    Add leaderboard for women

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,21 @@
 class StaticPagesController < ApplicationController
   def home
-      @reports = Report.order(created_at: :desc).limit(4)
-      @photos = Photo.order(created_at: :desc).limit(5)
-      @teams = Team.order(points: :desc)
-      @current_match = Match.where(result: -2).first
+    @reports = Report.order(created_at: :desc).limit(4)
+    @photos = Photo.order(created_at: :desc).limit(5)
+    @men_teams = Team.men.order(points: :desc)
+    @women_teams = Team.women.order(points: :desc)
+    @current_match = Match.where(result: -2).first
 
-      if @current_match
-        @live_score = @current_match.live_scores.last
-      end
+    if @current_match
+      @live_score = @current_match.live_scores.last
+    end
 
-      @top_scorers = Player.order("goals_count desc").limit(3)
-      @matches_ended = Match.where.not(result: -2).count
+    @top_scorers = Player.order(goals_count: :desc).limit(3)
+    @matches_ended = Match.where.not(result: -2).count
 
-      #static content
-      @previous_top_scorers = Player.all.sample(3)
-
+    # static content
+    @previous_top_scorers = Player.all.sample(3)
   end
 
-  def about
-  end
+  def about; end
 end
